ae7f84a58462269b6ceded8609bd7efa9c47e388
Moritz Rumpf <moritz.rumpf@gmail.com>
2020-11-25 14:32:38 +0100

    Remove redundant description of the kata

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,10 +5,4 @@ module Lib
 helloWorld :: IO ()
 helloWorld = putStrLn "Hello World!"
 
--- “Given an amount and a series of Coin Values, 
--- give me the best possible solution that requires the least amount of Coins 
--- and the remaining value if there is any”
-
--- Example: For the series of Coin Values [5 Cent, 10 Cent, 10 Cent, 20 Cent]
---  and the amount "25" I get back the solution [5 Cent, 20 Cent]
---   and the remaining amount 0
+-- Add your coin changer function here
