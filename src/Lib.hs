c7418f2156228bc4fe94b06233a9ecc2f4cc6e91
Moritz Rumpf <moritz.rumpf@gmail.com>
2022-01-11 09:23:00 +0100

    Add example implementation and tests

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,31 @@
 module Lib
-    ( helloWorld
-    ) where
+  ( helloWorld,
+    change,
+    Coins,
+  )
+where
+
+import Data.Function ((&))
+import Data.List (find, sort, (\\))
+import Data.Maybe (fromMaybe, listToMaybe)
 
 helloWorld :: IO ()
 helloWorld = putStrLn "Hello World!"
 
 -- Add your coin changer function here
+
+type Coins = [Int]
+
+change :: Int -> Coins -> (Coins, Int)
+change 0 availableCoins = ([], 0)
+change amount [] = ([], amount)
+change toChange availableCoins =
+  maybe
+    ([], toChange)
+    ( \coin ->
+        let (restChange, restRemainder) = change (toChange - coin) (availableCoins \\ [coin])
+         in (coin : restChange, restRemainder)
+    )
+    findLargestCoin
+  where
+    findLargestCoin = find (<= toChange) (reverse $ sort availableCoins)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,3 +1,6 @@
+import Control.Monad (sequence)
+import Data.List ((\\))
+import Lib (Coins, change)
 import Test.Hspec
 import Test.QuickCheck
 
@@ -7,3 +10,23 @@ main = hspec $ do
     it "reversing a String twice yields the original String" $ do
       -- https://hspec.github.io/quickcheck.html
       property $ \(x :: String) -> reverse (reverse x) == x
+
+  describe "coin changer" $ do
+    it "returns the amount when there are no coins available" $ do
+      property $ \(Positive amount) -> change amount [] `shouldBe` ([], amount)
+
+    it "returns the coin with no remainder when there is a coin that equals the amount" $ do
+      property $ \(Positive amount) -> change amount [amount] `shouldBe` ([amount], 0)
+
+    it "remainder and coins sum up to the amount" $ do
+      property $ \(positiveCoins :: [Positive Int], Positive amount) ->
+        let coins = map getPositive positiveCoins
+            (resultCoins, resultRemainder) = change amount coins
+         in sum resultCoins + resultRemainder == amount
+
+    it "only leaves coins unused that are bigger than the remaining amount" $ do
+      property $ \(positiveCoins :: [Positive Int], Positive amount) ->
+        let coins = map getPositive positiveCoins
+            (resultCoins, resultRemainder) = change amount coins
+            remainingCoins = coins \\ resultCoins
+         in remainingCoins `shouldSatisfy` all (> resultRemainder)
